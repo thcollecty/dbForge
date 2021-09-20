e21f96fd28d77270dbee5c4a17a401be10cf5309
Danial <danicircle360@gmail.com>
2021-09-20 04:47:39 -0700

    add new table, named department

--- Tables/dbo.Department_tbl.sql
+++ Tables/dbo.Department_tbl.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Department_tbl] (
   [Id] [int] NOT NULL,
   [Name] [nvarchar](50) NOT NULL,
+  [Contact] [int] NOT NULL,
   CONSTRAINT [PK_Department_tbl] PRIMARY KEY CLUSTERED ([Id])
 )
 ON [PRIMARY]
